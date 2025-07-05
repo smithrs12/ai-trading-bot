31b449c1a399cd3b830649dabe262a4453fa0adb
Ryan Smith <Rchristoph99@outlook.com>
2025-07-04 22:32:34 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-python3 main.py
+
+MODE=${MODE:-main}
+
+if [[ "$MODE" == "main" ]]; then
+    exec python main.py
+elif [[ "$MODE" == "worker" ]]; then
+    exec python worker.py
+elif [[ "$MODE" == "dashboard" ]]; then
+    exec streamlit run dashboard.py --server.port=5000 --server.enableCORS=false
+else
+    echo "Unknown mode: $MODE"
+    exit 1
+fi
